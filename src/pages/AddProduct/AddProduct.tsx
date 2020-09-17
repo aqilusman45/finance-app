e0723d836a671f945eb69880ea486a6bf1ec71f8
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-09-17 22:58:41 +0500

    page completed

--- src/pages/AddProduct/AddProduct.tsx
+++ src/pages/AddProduct/AddProduct.tsx
@@ -1,44 +1,88 @@
 import React from 'react'
 import './AddProduct.css'
 import NewIonMenu from './../IonMenu/NewIonMenu'
-import { IonGrid, IonRow, IonCol, IonContent, IonItem, IonLabel, IonInput } from '@ionic/react';
+import img from './../images/moble.jpeg'
+import { IonGrid, IonRow, IonCol, IonContent, IonItem, IonLabel, IonInput, IonImg, } from '@ionic/react';
 
 const AddProduct = () => {
     return (
         <>
 
-            <IonContent className="ion-text-center ion-padding">
-                <NewIonMenu/>
+            <IonContent className="ion-padding">
                 <IonGrid>
                     <IonRow>
-                    <IonCol>ion-col</IonCol>
+                        <IonCol size="1"></IonCol>
+
                         <form className="displayContent">
-                            <IonCol>
-                                <IonItem>
+
+                            <IonCol size="3">
+                                <IonItem className="InutField">
                                     <IonLabel position="stacked">Product Name *</IonLabel>
                                     <IonInput type="text" required placeholder="Name"> </IonInput>
                                 </IonItem>
-                                <IonItem>
+                                <IonItem className="InutField">
                                     <IonLabel position="stacked">Cost Price *</IonLabel>
                                     <IonInput type="number" required placeholder="100"> </IonInput>
                                 </IonItem>
-                                <IonItem>
+                                <IonItem className="InutField">
                                     <IonLabel position="stacked">List Price *</IonLabel>
                                     <IonInput type="number" required placeholder="200"> </IonInput>
                                 </IonItem>
-                                <IonItem>
+                                <IonItem className="InutField">
                                     <IonLabel position="stacked">Quantity *</IonLabel>
                                     <IonInput type="number" required placeholder="100"> </IonInput>
                                 </IonItem>
 
                             </IonCol>
-                            <IonCol>
-                            <IonItem>
+                            <IonCol size="3">
+                                <IonItem className="InutField">
                                     <IonLabel position="stacked">Discription</IonLabel>
                                     <IonInput type="text" required placeholder="(Variant Specs)"> </IonInput>
                                 </IonItem>
+                                <div className="InutField">
+                                    <select required name="cars" className="selectBox">
+                                        <option value="volvo">volvo</option>
+                                        <option value="saab">Saab</option>
+                                        <option value="mercedes">Mercedes</option>
+                                        <option value="audi">Audi</option>
+                                    </select>
+
+                                    <button className="globalBtn">Add new Brand</button>
+                                    {/* <IonButton color="success"></IonButton> */}
+                                </div>
+                                <div className="InutField">
+
+                                    <select required name="cars" className="selectBox">
+                                        <option value="volvo">Volvo</option>
+                                        <option value="saab">Saab</option>
+                                        <option value="mercedes">Mercedes</option>
+                                        <option value="audi">Audi</option>
+                                    </select>
+
+                                    <button className="globalBtn">Add new Category</button>
+
+                                </div>
                             </IonCol>
-                            <IonCol>ion-col</IonCol>
+                            <IonCol size="4" className="InutField">
+
+                                <input type="file" />
+
+                                <div className="borderDiv">
+
+                                    {/* <img src={img}/> */}
+                                    <IonImg className="imgSize" src={img} />
+                                </div>
+                                <div className="ion-text-end">
+
+                                <button className="globalBtn">Add new Category</button>
+                                </div>
+
+                            </IonCol>
+                            <IonCol size="1">
+
+                            </IonCol>
+
+
                         </form>
                     </IonRow>
 
