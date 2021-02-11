040ad84b27fa97b201da40c1acc0064fb485cc62
Huzaifa Aslam <huzaifaslam45@gmail.com>
2021-02-11 14:59:43 -0800

    changed UI for manage Invoice

--- src/components/ManageInvoices/ManageInvoices.tsx
+++ src/components/ManageInvoices/ManageInvoices.tsx
@@ -5,10 +5,15 @@ import {
   IonCol,
   IonContent,
   IonSearchbar,
+  IonPage,
+  IonItem,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
 } from "@ionic/react";
 import InoviceViewModel from "../InvoiceViewModel/InoviceViewModel";
 import Table from "react-bootstrap/Table";
-
+import "./ManageInvoices.css";
 import { IEditInvoice } from "../../lib/editInvoice";
 const invoice1 = {
   invoiceID: "234",
@@ -32,73 +37,103 @@ const keys = ["Invoice ID", "Name", "Phone", "Discount", "Tax", "Total"];
 const ManageInvoices: React.FC = () => {
   const [showModel, setShowModel] = useState<boolean>(false);
   const [invoice, setInvoice] = useState<IEditInvoice[] | null>();
+  const [gender, setGender] = useState<string>("all");
 
   const [invoices] = useState<any>([{ ...invoice1 }, { ...invoice2 }]);
   return (
-    <IonContent>
-      <InoviceViewModel
-        invoice={invoice}
-        showModel={showModel}
-        setShowModel={setShowModel}
-      />
-      <IonGrid>
-        <IonRow>
-          <IonCol size="12">
-            <IonSearchbar showCancelButton="focus" debounce={1000} />
-          </IonCol>
-        </IonRow>
-        {/* <IonLoading isOpen={isLoading} message={"Please wait..."} /> */}
-        {!!invoices?.length ? (
-          <>
-            <IonRow>
-              <IonCol size="12">
-                <Table striped hover variant="dark" responsive="sm">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      {keys.map((heading) => {
-                        return <th key={heading}>{heading.toUpperCase()}</th>;
+    <IonPage>
+      <IonContent>
+        <InoviceViewModel
+          invoice={invoice}
+          showModel={showModel}
+          setShowModel={setShowModel}
+        />
+        <IonGrid className="ion-margin">
+          <IonRow>
+            <IonCol size="12">
+              <IonSearchbar showCancelButton="focus" debounce={1000} />
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-margin">
+            <IonCol size="6">
+              <input
+                className="dateInputStyle shadow-sm"
+                type="date"
+                name="dateFrom"
+              />
+              <span className="dateFilterSpan">To</span>
+              <input
+                className="dateInputStyle shadow-sm"
+                type="date"
+                name="dateTill"
+              />
+            </IonCol>
+            <IonCol size="6">
+              <IonItem>
+                <IonLabel>PAYMENT</IonLabel>
+                <IonSelect
+                  value={gender}
+                  onIonChange={(e) => setGender(e.detail.value)}
+                >
+                  <IonSelectOption value="all">All</IonSelectOption>
+                  <IonSelectOption value="bank">Bank</IonSelectOption>
+                </IonSelect>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          {/* <IonLoading isOpen={isLoading} message={"Please wait..."} /> */}
+          {!!invoices?.length ? (
+            <>
+              <IonRow>
+                <IonCol size="12">
+                  <Table striped hover variant="dark" responsive="sm">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        {keys.map((heading) => {
+                          return <th key={heading}>{heading.toUpperCase()}</th>;
+                        })}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {invoices.map((invoice: any, index: number) => {
+                        return (
+                          <tr
+                            key={index}
+                            className="table-row-hover"
+                            onClick={() => {
+                              console.log("open model");
+
+                              setInvoice(() =>
+                                invoices.find(
+                                  (account: any) =>
+                                    account.invoiceID === invoice.invoiceID
+                                )
+                              );
+                              setShowModel(!showModel);
+                            }}
+                          >
+                            <td>{index + 1}</td>
+                            <td>{invoice.invoiceID}</td>
+                            <td>{invoice.userName}</td>
+                            <td>{invoice.phone}</td>
+                            <td>{invoice.discount} %</td>
+                            <td>{invoice.tax} %</td>
+                            <td>{invoice.total}</td>
+                          </tr>
+                        );
                       })}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {invoices.map((invoice: any, index: number) => {
-                      return (
-                        <tr
-                          key={index}
-                          className="table-row-hover"
-                          onClick={() => {
-                            console.log("open model");
-                            
-                            setInvoice(() =>
-                              invoices.find(
-                                (account: any) =>
-                                  account.invoiceID === invoice.invoiceID
-                              )
-                            );
-                            setShowModel(!showModel);
-                          }}
-                        >
-                          <td>{index + 1}</td>
-                          <td>{invoice.invoiceID}</td>
-                          <td>{invoice.userName}</td>
-                          <td>{invoice.phone}</td>
-                          <td>{invoice.discount} %</td>
-                          <td>{invoice.tax} %</td>
-                          <td>{invoice.total}</td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </Table>
-              </IonCol>
-            </IonRow>
-          </>
-        ) : (
-          <p>No Invoice found</p>
-        )}
-      </IonGrid>
-    </IonContent>
+                    </tbody>
+                  </Table>
+                </IonCol>
+              </IonRow>
+            </>
+          ) : (
+            <p>No Invoice found</p>
+          )}
+        </IonGrid>
+      </IonContent>
+    </IonPage>
   );
 };
 export default ManageInvoices;
